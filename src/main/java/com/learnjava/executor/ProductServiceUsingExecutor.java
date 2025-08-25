ee47a266b8b58e0de620dba95e5240a33bcda8e6
Carlos Castañeda <dario20049@gmail.com>
2025-08-25 18:14:59 +0900

    ProductServiceUsingExecutor

---
+++ src/main/java/com/learnjava/executor/ProductServiceUsingExecutor.java
@@ -0,0 +1,64 @@
+package com.learnjava.executor;
+
+import static com.learnjava.util.CommonUtil.stopWatch;
+import static com.learnjava.util.LoggerUtil.log;
+
+import com.learnjava.domain.Product;
+import com.learnjava.domain.ProductInfo;
+import com.learnjava.domain.Review;
+import com.learnjava.service.ProductInfoService;
+import com.learnjava.service.ProductService;
+import com.learnjava.service.ReviewService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ProductServiceUsingExecutor {
+
+  // EXECUTOR SERVICE
+  static ExecutorService executorService =
+      Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+  private ProductInfoService productInfoService;
+  private ReviewService reviewService;
+
+  public ProductServiceUsingExecutor(ProductInfoService productInfoService,
+      ReviewService reviewService) {
+    this.productInfoService = productInfoService;
+    this.reviewService = reviewService;
+  }
+
+  public Product retrieveProductDetails(String productId)
+      throws ExecutionException, InterruptedException {
+    stopWatch.start();
+
+    //EXECUTOR SERVICE, callable
+    Future<ProductInfo> productInfoFuture = executorService.submit(
+        () -> productInfoService.retrieveProductInfo(productId));
+
+    Future<Review> reviewFuture =
+        executorService.submit(
+            () -> reviewService.retrieveReviews(productId)
+        );
+
+    ProductInfo productInfo = productInfoFuture.get();
+    Review review = reviewFuture.get();
+
+    stopWatch.stop();
+    log("Total Time Taken : " + stopWatch.getTime());
+    return new Product(productId, productInfo, review);
+  }
+
+  public static void main(String[] args) {
+
+    ProductInfoService productInfoService = new ProductInfoService();
+    ReviewService reviewService = new ReviewService();
+    ProductService productService = new ProductService(productInfoService, reviewService);
+    String productId = "ABC123";
+    Product product = productService.retrieveProductDetails(productId);
+    log("Product is " + product);
+
+    executorService.shutdown();
+  }
+}
